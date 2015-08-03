c745067c924e4342bb32ee939179d51b2c2f5a41
Christopher Alcock <christopher.alcock@gmail.com>
2015-08-03 14:28:12 +0100

    end of part 6

--- hello.rb
+++ hello.rb
@@ -1,10 +1,15 @@
 require 'sinatra'
 
 get '/' do
-  @name = %w(Idiot Fucker Cockface Twat).sample
+  @name = %w(Nice_Cat Really_Nice_Cat Really_Just_The_Loveliest_Cat).sample
   erb :index
 end
 
 get '/secret' do
   'Big Secret Page (ssh!)'
+end
+
+get '/hello' do
+  @visitor = params[:name]
+  erb :index
 end
